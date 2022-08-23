700134df6d5a176db8027ad34c1f596f857110c6
Bhaskar Ram <bhaskarvilles@duck.com>
2022-08-23 16:11:53 +0530

    update to oop with my age

--- oop.js
+++ oop.js
@@ -6,4 +6,15 @@ const MyName = {
         return this.firstname + ' ' + this.middlename + ' ' + this.lastname;
     }
 }
-console.log(MyName.fullname());
+console.log( ("Hello World, My name is") + ' ' + MyName.fullname());
+
+const Myage = {
+    DateofBirth : "05/08/1999",
+    Year : "1999",
+    Date : "05",
+    Month : "August",
+    Fulldob : function(){
+        return this.Date + ' ' + this.Month + ' ' + this.Year;
+    }
+}
+console.log( ("My Date of Birth is") + ' ' + Myage.Fulldob());
