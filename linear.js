037daea006e7b56193273ba39c4b3775064340c9
Bhaskar Ram <bhaskarvilles@duck.com>
2022-08-23 16:01:38 +0530

    linear update

---
+++ linear.js
@@ -0,0 +1,31 @@
+function linearsearch(value, list) {
+    let found = false;
+    let position = -1;
+    let index = 0;
+
+    while(!found && index < list.length) {
+        if(list[index] == value){
+            found = true;
+            position = index;
+        } else {
+            index += 1;
+        }
+    }
+    return position; 
+}
+
+function linearsearch(value, list) {
+    let found = false;
+    let position = -1;
+    let index = 0;
+
+    while(!found && index < list.length) {
+        if(list[index] == value){
+            found = true;
+            position = index;
+        } else {
+            index += 1;
+        }
+    }
+    return position;
+}
